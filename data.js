73a4a634a8506063ec0396cfab0801aa2dff6af4
Jason <jasonc.chiu@mail.utoronto.ca>
2026-06-08 13:46:03 -0400

    Update player data and history entries
    Updated player profiles with new stats and added history entries.

--- data.js
+++ data.js
@@ -4,21 +4,23 @@
 var BALL_RUN_DATA = {
     players: [
         // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
-        ["Calvin", "Carry+", "2-0", "6'1\"", "180 lbs", "6'3\"", ""],
-        ["Karnveer", "Mid", "2-0", "--", "--", "--", ""],
+        ["Calvin", "Carry+", "2-0", "5'8\"", "155 lbs", "5'5\"", ""],
+        ["Karnveer", "Mid", "2-0", "6'1", "215 lbs ", "6'2", ""],
         ["Theesiik", "Carry+", "0-1", "--", "--", "--", ""],
         ["Earl", "Body-", "1-1", "--", "--", "--", ""],
         ["Stefan", "Liability+", "0-0", "--", "--", "--", ""],
-        ["Thomas", "Cardio", "0-0", "--", "--", "--", ""],
+        ["Thomas", "Cardio", "0-0", "5'9", "175 lbs", "6'0", ""],
         ["Anish", "Body", "0-0", "--", "--", "--", ""],
         ["Mario", "yn-", "0-0", "--", "--", "--", ""],
         ["Krishaal", "mid", "0-0", "--", "--", "--", ""],
-        ["Gurjot", "Liability+", "0-1", "--", "--", "--", ""],
+        ["Gurjot", "Liability+", "0-1", "6'1", "205 lbs", "6'2", ""],
         ["Ethan", "Mid", "0-1", "--", "--", "--", ""],
-        ["Jason", "Carry-", "0-1", "--", "--", "--", ""]
+        ["Jason", "Carry-", "0-1", "5'10", "160 lbs", "5'11", ""]
     ],
     history: [
         // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Added Player profiles, and updated stats", date "6/8/2026", author: "TS, KM", reviewers: "GK, CB, TN"},
+        { desc: "Created Website", date: "6/82026", author: "TS", reviewers: "" },
         { desc: "Added Krishaal", date: "6/8/2026", author: "KM", reviewers: "CB" },
         { desc: "Jason to Carry- & TN to Cardio", date: "6/6/2026", author: "TS", reviewers: "EDSG, TN" },
         { desc: "Tier Change - Legend to yn", date: "6/6/2026", author: "KM", reviewers: "" },
